5306d6070f50e8340ebaec3da39d583f31147600
Weiming Zhai <wmzhai@gmail.com>
2016-02-23 20:56:04 +0800

    deploy

--- resources/deploy.sh
+++ resources/deploy.sh
@@ -21,4 +21,4 @@ git commit -m "Deploy to Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "git@git.fitark.org:wmzhai/microservice.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "git@github.com:wmzhai/microservice.git" master:gh-pages > /dev/null 2>&1
